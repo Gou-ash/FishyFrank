b7d013087cd9420b0e3974cf523bf45e0fa1c9ba
szymonmlbb@gmail.com <szymonmlbb@gmail.com>
2025-05-17 07:21:27 +0200

    AI changes merge

--- app/src/main/java/com/example/hackaton/MainActivity.kt
+++ app/src/main/java/com/example/hackaton/MainActivity.kt
@@ -69,9 +69,6 @@ class MainActivity : ComponentActivity() {
 
 
         notification.createNotificationChannel()
-        setContent {
-            GeminiTest(ai)
-        }
     }
 
     // On returning to the app, check if the permission was granted and update usage
